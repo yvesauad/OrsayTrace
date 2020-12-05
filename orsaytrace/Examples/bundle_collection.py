630d10094f62e77d6d734bccac658bd987f0469d
yvesauad <yvesauad@gmail.com>
2020-12-05 01:42:13 +0100

    v1.0.33. Bundle Collection and parabolic aberration p2

--- orsaytrace/Examples/bundle_collection.py
+++ orsaytrace/Examples/bundle_collection.py
@@ -3,10 +3,12 @@ import numpy
 import matplotlib.pyplot as plt
 
 x, y, z, p, res = 5., 5., 5., 1.0, 0.05
+min_x, max_x = -1.0, 1.0
+min_z, max_z = 2.0-p/2.-0.75, 2.0-p/2.+0.25
 a = ot.Simu(x, y, z, res)
 
-for x in numpy.linspace(-0.5, 0.5, 50):
-    for z in numpy.linspace(2.0-p/2.-0.5, 2.0-p/2.+0.5, 50):
+for x in numpy.linspace(min_x, max_x, 75):
+    for z in numpy.linspace(min_z, max_z, 75):
         a.d2_flex_source(0.0, [x, 0, z], [0, -1, 0], 1.0, 7, 'point')
 
 a.create_parabolic_surface_element([0.0, 0, 2.0], -1.0, 2, 3.0, p)
@@ -15,7 +17,7 @@ a.create_rectangle_element([-x/2, x/2, 0-0.3, y/2, -z/2, z/2], 1.0, [0, 0, 0])
 a.create_analysis_plan([0, 0, 1], -2.0, reflection_count = (1, 1))
 #Lets create a bundle of plans
 it = 0
-tr = 2.0
+tr = 1.0
 xc, yc, zc = 0, -1.0, -2.0
 mesh = 2
 unitr = tr / mesh
@@ -39,7 +41,7 @@ y_new_list = [[ photon.init['pos'][1] for photon in pl[i].photons] for i in rang
 z_new_list = [[ photon.init['pos'][2] for photon in pl[i].photons] for i in range(len(pl))]
 
 fig, axes = plt.subplots(nrows=1, ncols=it, sharex=False, sharey=False)
-[axes[i].hist2d(x_new_list[i], z_new_list[i], bins=25, range=[[xc-tr, xc+tr], [yc-tr, yc+tr]]) for i in range(it)]
+[axes[i].hist2d(x_new_list[i], z_new_list[i], bins=25, range=[[min_x, max_x], [min_z, max_z]]) for i in range(it)]
 
 
 plt.show()
