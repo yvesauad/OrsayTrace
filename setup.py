c9cfda59b83302b2289f03c184169a90fc369bc8
yvesauad <yvesauad@gmail.com>
2020-12-09 09:06:06 +0100

    v1.1.11 Bundle collection fix.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ proj_urls = {
 setup(
         name='orsaytrace',
         packages = ['orsaytrace'],
-        version = '1.1.10',
+        version = '1.1.11',
         license = 'MIT',
         description = 'Flexible ray tracing for optics',
         long_description = open('README.rst').read(),
